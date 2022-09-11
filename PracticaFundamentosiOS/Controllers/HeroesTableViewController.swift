84e85bdfcfa7b4c6eea05f44da1655418a54d928
Roberto Rojo <rrojo.va@gmail.com>
2022-09-12 01:27:27 +0200

    Fixed name

--- PracticaFundamentosiOS/Controllers/HeroesTableViewController.swift
+++ PracticaFundamentosiOS/Controllers/HeroesTableViewController.swift
@@ -23,12 +23,12 @@ class HeroesTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 1
+        return 0
     }
 
     /*
