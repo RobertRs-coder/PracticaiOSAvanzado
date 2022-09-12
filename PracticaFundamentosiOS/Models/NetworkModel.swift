c5b6166da2785aaaabffc60dbb1a29ef04552e13
Roberto Rojo <rrojo.va@gmail.com>
2022-09-12 20:00:39 +0200

    Create dataTask and check nil error and noData

--- PracticaFundamentosiOS/Models/NetworkModel.swift
+++ PracticaFundamentosiOS/Models/NetworkModel.swift
@@ -10,19 +10,21 @@ import Foundation
 enum NetworkError: Error {
     case malformedURL
     case dataFormat
+    case otherError
+    case noData
 }
 
 class NetworkModel {
     let server = "https://vapor2022.herokuapp.com"
-    func login(user: String, password: String, completion: @escaping (String?, Error?) -> Void) {
+    func login(user: String, password: String, completion: @escaping (String?, NetworkError?) -> Void) {
         guard let url = URL(string: "\(server)/api/auth/login") else {
-            completion(nil, NetworkError.malformedURL)
+            completion(nil, .malformedURL)
             return
         }
         
         let loginString = String(format: "%@:%@", user, password)
         guard let loginData = loginString.data(using: .utf8) else {
-            completion(nil, NetworkError.dataFormat)
+            completion(nil, .dataFormat)
             return
         }
         
@@ -32,6 +34,18 @@ class NetworkModel {
         urlRequest.httpMethod = "POST"
         urlRequest.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
         
-        
+        let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            guard error == nil else {
+                completion(nil, .otherError)
+                return
+            }
+            
+            guard let data = data else {
+                completion(nil, .noData)
+                return
+            }
+            
+      
+        }
     }
 }
