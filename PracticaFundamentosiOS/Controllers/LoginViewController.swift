0e6710208a5392ddd7fab9818470beb7bdf5b392
Roberto Rojo <rrojo.va@gmail.com>
2022-09-12 20:39:19 +0200

    Create activityIndicator

--- PracticaFundamentosiOS/Controllers/LoginViewController.swift
+++ PracticaFundamentosiOS/Controllers/LoginViewController.swift
@@ -15,25 +15,15 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var userTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    //MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Heroes"
         
-        
         // Do any additional setup after loading the view.
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     //MARK: IBActions
     @IBAction func loginOnTap(_ sender: UIButton) {
         let model = NetworkModel()
@@ -42,15 +32,16 @@ class LoginViewController: UIViewController {
         
         guard !user.isEmpty, !password.isEmpty else { return }
         
-        model.login(user: user, password: password) { token, _ in
+        model.login(user: user, password: password) { [weak self] token, _ in
             print("Your token is: \(token ?? "")")
             
             guard let token = token, !token.isEmpty else {
                 return
             }
+            
             DispatchQueue.main.async {
                 let nextViewController = HeroesTableViewController()
-                self.navigationController?.setViewControllers([nextViewController], animated: true)
+                self?.navigationController?.setViewControllers([nextViewController], animated: true)
             }
         }
     }
