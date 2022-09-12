89be96e72fb797f34625ccad55273c67e0071c2b
Roberto Rojo <rrojo.va@gmail.com>
2022-09-12 19:51:04 +0200

    Create urlRequest

--- PracticaFundamentosiOS/Models/NetworkModel.swift
+++ PracticaFundamentosiOS/Models/NetworkModel.swift
@@ -9,16 +9,25 @@ import Foundation
 
 enum NetworkError: Error {
     case malformedURL
+    case dataFormat
 }
 
 class NetworkModel {
     let server = "https://vapor2022.herokuapp.com"
-    func login(user: String, pasword: String, completion: @escaping (String?, Error?) -> Void) {
+    func login(user: String, password: String, completion: @escaping (String?, Error?) -> Void) {
         guard let url = URL(string: "\(server)/api/auth/login") else {
             completion(nil, NetworkError.malformedURL)
             return
         }
         
+        let loginString = String(format: "%@:%@", user, password)
+        guard let loginData = loginString.data(using: .utf8) else {
+            completion(nil, NetworkError.dataFormat)
+            return
+        }
         
+        let base64LoginString = loginData.base64EncodedString()
+        
+        var urlRequest
     }
 }
